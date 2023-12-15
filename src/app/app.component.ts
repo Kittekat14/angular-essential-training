a1ff2767d9510ee9e08d36c8eff4e96ae98210ab
Kittekat14 <83455469+Kittekat14@users.noreply.github.com>
2023-12-15 22:55:17 +0100

    bootstrapped intitial Angular app part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: "<h1>My App</h1>",
+  templateUrl: "./app.component.html", // relative path
+  styleUrls: ["app.component.css"],
 })
 export class AppComponent {}
